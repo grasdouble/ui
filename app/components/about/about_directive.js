5d00786980a9594ff04c868599713e9d1c63e4b7
NooFr <dev.n2ofr@gmail.com>
2015-04-13 02:49:11 +0200

    (suite) : Ajout du about + mise en place d'un slide pour l'xp + fond en dégradé (à voir si on garde)

---
+++ app/components/about/about_directive.js
@@ -0,0 +1,25 @@
+/**
+ * Created by sebastien on 09/04/15.
+ */
+angular.module('SlmApp').directive('slmAbout', function () {
+    'use strict';
+    return {
+        restrict: 'E',
+        //priority: 0,
+        templateUrl: 'app/components/about/about.html',
+        //replace: bool,
+        //transclude: bool,
+        scope: true,
+        controller: "AboutController",
+        controllerAs: "about"
+        //require: string,
+        //link: function postLink(scope, iElement, iAttrs) {},
+        //compile: function compile(tElement, tAttrs, transclude) {
+        //    return {
+        //        pre: function preLink(scope, iElement, iAttrs, controller) {},
+        //        post: function postLink(scope, iElement, iAttrs, controller) {}
+        //    }
+        //}
+
+    };
+});
