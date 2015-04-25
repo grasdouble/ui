7960b300e78ba82d341d4fabbd8cac39751704ab
NooFr <dev.n2ofr@gmail.com>
2015-04-25 22:46:40 +0200

    Suppression du module ngSticky pour un fonctionnement via le controller

--- app/views/home/components/menu/menu_controller.js
+++ app/views/home/components/menu/menu_controller.js
@@ -9,13 +9,13 @@ angular.module('SlmApp').
 
         var _this = this;
 
-        //var headerElement = document.getElementById("sroHeader");
+
         var positionY = -1;
         this.menuFixed = false;
 
         var onScroll = function onScroll() {
             positionY = window.scrollY;
-            //headerElement.style.backgroundPositionY = -positionY / 3 + "px";
+
             if (positionY > 460) {
                 if (_this.menuFixed === false) {
                     _this.menuFixed = true;
