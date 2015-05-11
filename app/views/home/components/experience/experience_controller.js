5543b036960ef1f477e7c4c5ab46f9f69e319db1
Sébastien LE MOUILLOUR <dev.n2ofr@gmail.com>
2015-05-11 14:40:30 +0200

    [Experience]Ajout d'une fonction slide au chargement de la page

--- app/views/home/components/experience/experience_controller.js
+++ app/views/home/components/experience/experience_controller.js
@@ -1,19 +1,36 @@
 /**
  * Created by sebastien on 09/04/15.
  */
-angular.module('SlmApp').controller('ExperienceController',
-    function ($sce, ExperienceService) {
+angular.module('SlmApp')
+    .controller('ExperienceController',
+    function ($sce, $interval, $scope, ExperienceService) {
 
         this.validHTML = function validHTML(value) {
             return $sce.trustAsHtml(value);
         };
 
-        this.type = "Pro";
-        this.projetPro = "ERDF";
-        this.projetPerso = "CV-Web";
-        this.vignetteActive = 0;
+        var _this = this;
 
+        this.type = "Pro";
+        this.vignetteActive = 0;
         this.experiences = ExperienceService.getDonnees();
+        this.INTERVAL = 5000;
+        this.slide = null;
+        this.stopSlide = false;
+
+        this.nextSlide = function nextSlide() {
+            if (_this.stopSlide) {
+                $interval.cancel(_this.slide);
+            } else {
+                _this.vignetteActive = (_this.vignetteActive < 3) ? ++_this.vignetteActive : 0;
+            }
+        };
+
+        this.loadSlides = function loadSlides() {
+            this.slide = $interval(this.nextSlide, this.INTERVAL);
+        };
+
+        this.loadSlides();
 
     }
 );
