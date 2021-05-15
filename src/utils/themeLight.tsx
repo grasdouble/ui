b564e73d3d6694da7447f2700ecbeefa8a52f6cb
Noofreuuuh <dev.n2ofr@gmail.com>
2021-05-15 15:09:14 +0200

    create commonOptions for theme

--- src/utils/themeLight.tsx
+++ src/utils/themeLight.tsx
@@ -1,58 +1,28 @@
 import { createMuiTheme, responsiveFontSizes } from "@material-ui/core/styles";
+// @ts-ignore
+import { deepmerge } from "@material-ui/utils";
+import { themeCommonOptions } from "./themeCommonOptions";
 
-let themeLight = createMuiTheme({
-  palette: {
-    type: "light",
-    primary: {
-      light: "#7986cb",
-      main: "#3f51b5",
-      dark: "#303f9f",
-      contrastText: "#fff",
-    },
-    secondary: {
-      light: "#ff4081",
-      main: "#f50057",
-      dark: "#c51162",
-      contrastText: "#fff",
-    },
-    error: {
-      light: "#e57373",
-      main: "#f44336",
-      dark: "#d32f2f",
-      contrastText: "#fff",
-    },
-    warning: {
-      light: "#ffb74d",
-      main: "#ff9800",
-      dark: "#f57c00",
-      contrastText: "rgba(0, 0, 0, 0.87)",
-    },
-    info: {
-      light: "#64b5f6",
-      main: "#2196f3",
-      dark: "#1976d2",
-      contrastText: "#fff",
-    },
-    success: {
-      light: "#81c784",
-      main: "#4caf50",
-      dark: "#388e3c",
-      contrastText: "rgba(0, 0, 0, 0.87)",
-    },
-    text: {
-      primary: "rgba(0, 0, 0, 0.87)",
-      secondary: "rgba(0, 0, 0, 0.54)",
-      disabled: "rgba(0, 0, 0, 0.38)",
-      hint: "rgba(0, 0, 0, 0.38)",
-    },
-    divider: "rgba(0, 0, 0, 0.12)",
-    background: {
-      paper: "#fff",
-      default: "#fafafa",
+let themeLightOptions = deepmerge(
+  {
+    palette: {
+      type: "light",
+      text: {
+        primary: "rgba(0, 0, 0, 0.87)",
+        secondary: "rgba(0, 0, 0, 0.54)",
+        disabled: "rgba(0, 0, 0, 0.38)",
+        hint: "rgba(0, 0, 0, 0.38)",
+      },
+      divider: "rgba(0, 0, 0, 0.12)",
+      background: {
+        paper: "#fff",
+        default: "#fff",
+      },
     },
   },
-});
-
+  themeCommonOptions
+);
+let themeLight = createMuiTheme(themeLightOptions);
 themeLight = responsiveFontSizes(themeLight);
 
 export { themeLight };
