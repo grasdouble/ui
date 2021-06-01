a015347fcebe4ee24dd97486bf7ee3db3afa3a04
Noofreuuuh <dev.n2ofr@gmail.com>
2021-06-02 00:48:12 +0200

    fix lint

--- src/pages/AboutMe/AboutMe.tsx
+++ src/pages/AboutMe/AboutMe.tsx
@@ -3,7 +3,6 @@ import { makeStyles, createStyles, Theme } from "@material-ui/core/styles";
 
 import Typography from "@material-ui/core/Typography";
 import Grid from "@material-ui/core/Grid";
-import Container from "@material-ui/core/Container";
 import Divider from "@material-ui/core/Divider";
 
 import MainTemplate from "layouts/Main";
